53ec6b484115a1e8e89598e2d8b0e6dfb9c8c9cb
Bradley Cicenas <bradley.cicenas@gmail.com>
2016-11-07 06:17:58 +0000

    move keymap to own file

--- main.go
+++ main.go
@@ -6,57 +6,13 @@ import (
 	"strings"
 )
 
-var NatoMap = map[string]string{
-	"-":        "Dash",
-	"0":        "Zero",
-	"1":        "One",
-	"2":        "Two",
-	"3":        "Three",
-	"4":        "Four",
-	"5":        "Five",
-	"6":        "Six",
-	"7":        "Seven",
-	"8":        "Eight",
-	"9":        "Niner",
-	"a":        "Alfa",
-	"b":        "Bravo",
-	"c":        "Charlie",
-	"d":        "Delta",
-	"e":        "Echo",
-	"f":        "Foxtrot",
-	"g":        "Golf",
-	"h":        "Hotel",
-	"i":        "India",
-	"j":        "Juliett",
-	"k":        "Kilo",
-	"l":        "Lima",
-	"m":        "Mike",
-	"n":        "November",
-	"o":        "Oscar",
-	"p":        "Papa",
-	"q":        "Quebec",
-	"r":        "Romeo",
-	"s":        "Sierra",
-	"t":        "Tango",
-	"u":        "Uniform",
-	"v":        "Victor",
-	"w":        "Whiskey",
-	"x":        "X-ray or Xray",
-	"y":        "Yankee",
-	"z":        "Zulu",
-	".":        "Point",
-	"EOL":      "Stop",
-	"hundred":  "Hundred",
-	"thousand": "Thousand",
-}
-
 type Acrophone struct {
 	charMap map[string]string
 }
 
-func (a *Acrophone) lookup(char string) (string, error) {
+func (a *Acrophone) Lookup(char string) (string, error) {
 	if len(char) > 1 {
-		return "", fmt.Errorf("lookup() accepts single character argument only")
+		return "", fmt.Errorf("Lookup() accepts single character argument only")
 	}
 	if res, ok := a.charMap[char]; ok == false {
 		return "", fmt.Errorf("unable to find match for character: %s", char)
@@ -77,7 +33,7 @@ func main() {
 
 	input := strings.Join(os.Args[1:], "")
 	for _, char := range input {
-		result, err := a.lookup(string(char))
+		result, err := a.Lookup(string(char))
 		if err != nil {
 			panic(err)
 		}
